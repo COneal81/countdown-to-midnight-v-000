517bfcb6a91b15e00e90e16eeda44519d9ce2958
Kari O'Neal <triadfirewood@gmail.com>
2018-07-26 12:40:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
 def countdown = 10
-   if while countdown < 10
+   while countdown < 10
      return "#{number}"
    countdown -= 1
-  else return "HAPPY NEW YEAR!"
-end
+  return "HAPPY NEW YEAR!"
 end
 end
