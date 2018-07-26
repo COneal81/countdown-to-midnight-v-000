e7ae189bbf2d0ffa09285630cb1fd139831dd1e7
Kari O'Neal <triadfirewood@gmail.com>
2018-07-26 12:50:19 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,8 @@ def countdown(seconds)
    while seconds > 0
      puts "#{number} SECONDS!"
    seconds -= 1
-  return "HAPPY NEW YEAR!"
   end
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(seconds)
